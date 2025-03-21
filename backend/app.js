1f14bfad4fe5037e6b928dcfdec984f2cea65bde
sumitst05 <sumitst05@gmail.com>
2025-03-21 21:12:30 +0530

    update: integrate db and serve static content

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,34 @@
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import path from "path";
 import "dotenv/config";
 
 const app = express();
 
-const port = process.env.PORT || 3000;
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use("/api", (req, res) => {
-	res.status(200).json({ result: "Server is working!" });
+  res.status(200).json({ result: "Server is working!" });
 });
 
+const __dirname = path.resolve();
 
-app.listen(port, () => {
-	console.log(` Listening on port ${port}... `);
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Error while connecting to database: ${err}`);
+  });
